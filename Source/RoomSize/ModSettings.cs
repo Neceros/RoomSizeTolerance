0e8e71e07a91c3f2df2f8b88be510b8748374864
Neceros <neceros@gmail.com>
2020-04-15 14:04:39 -0700

    update

---
+++ Source/RoomSize/ModSettings.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using UnityEngine;
+using Verse;
+
+namespace RoomSize
+{
+  public class RSModSettings : ModSettings
+  {
+    public static int amountRatherTight = 11;
+    public static int amountAverageSized = 21;
+    public static int amountSomewhatSpacious = 34;
+    public static int amountQuiteSpacious = 55;
+    public static int amountVerySpacious = 80;
+    public static int amountExtremelyspacious = 135;
+
+
+    public override void ExposeData()
+    {
+      base.ExposeData();
+      Scribe_Values.Look(ref amountRatherTight, "amountRatherTight");
+      Scribe_Values.Look(ref amountAverageSized, "amountAverageSized");
+      Scribe_Values.Look(ref amountSomewhatSpacious, "amountSomewhatSpacious");
+      Scribe_Values.Look(ref amountQuiteSpacious, "amountQuiteSpacious");
+      Scribe_Values.Look(ref amountVerySpacious, "amountVerySpacious");
+      Scribe_Values.Look(ref amountExtremelyspacious, "amountExtremelyspacious");
+    }
+  }
+
+  public class RSMod : Mod
+  {
+    RSModSettings settings;
+    public RSMod(ModContentPack con) : base(con)
+    {
+      this.settings = GetSettings<RSModSettings>();
+    }
+
+    public bool resetPressed;
+
+    public override void DoSettingsWindowContents(Rect inRect)
+    {
+      if (resetPressed)
+      {
+        RSModSettings.amountRatherTight = 11;
+        RSModSettings.amountAverageSized = 21;
+        RSModSettings.amountSomewhatSpacious = 34;
+        RSModSettings.amountQuiteSpacious = 55;
+        RSModSettings.amountVerySpacious = 80;
+        RSModSettings.amountExtremelyspacious = 135;
+        resetPressed = false;
+      }
+
+      Listing_Standard listing = new Listing_Standard();
+      listing.Begin(inRect);
+      listing.Label("RSRTLabel".Translate() + ": " + RSModSettings.amountRatherTight.ToString(), tooltip: "RSTooltip".Translate());
+      RSModSettings.amountRatherTight = (int)listing.Slider(RSModSettings.amountRatherTight, 1, RSModSettings.amountAverageSized - 1);
+      listing.Gap();
+      listing.Label("RSASLabel".Translate() + ": " + RSModSettings.amountAverageSized.ToString(), tooltip: "RSTooltip".Translate());
+      RSModSettings.amountAverageSized = (int)listing.Slider(RSModSettings.amountAverageSized, RSModSettings.amountRatherTight + 1, RSModSettings.amountSomewhatSpacious - 1);
+      listing.Gap();
+      listing.Label("RSSSLabel".Translate() + ": " + RSModSettings.amountSomewhatSpacious.ToString(), tooltip: "RSTooltip".Translate());
+      RSModSettings.amountSomewhatSpacious = (int)listing.Slider(RSModSettings.amountSomewhatSpacious, RSModSettings.amountAverageSized + 1, RSModSettings.amountQuiteSpacious - 1);
+      listing.Gap();
+      listing.Label("RSQSLabel".Translate() + ": " + RSModSettings.amountQuiteSpacious.ToString(), tooltip: "RSTooltip".Translate());
+      RSModSettings.amountQuiteSpacious = (int)listing.Slider(RSModSettings.amountQuiteSpacious, RSModSettings.amountSomewhatSpacious + 1, RSModSettings.amountVerySpacious - 1);
+      listing.Gap();
+      listing.Label("RSVSLabel".Translate() + ": " + RSModSettings.amountVerySpacious.ToString(), tooltip: "RSTooltip".Translate());
+      RSModSettings.amountVerySpacious = (int)listing.Slider(RSModSettings.amountVerySpacious, RSModSettings.amountQuiteSpacious + 1, RSModSettings.amountExtremelyspacious - 1);
+      listing.Gap();
+      listing.Label("RSESLabel".Translate() + ": " + RSModSettings.amountExtremelyspacious.ToString(), tooltip: "RSTooltip".Translate());
+      RSModSettings.amountExtremelyspacious = (int)listing.Slider(RSModSettings.amountExtremelyspacious, RSModSettings.amountVerySpacious + 1, 500);
+      listing.Gap();
+      resetPressed = listing.ButtonText("RSResetLabel".Translate());
+      listing.End();
+      base.DoSettingsWindowContents(inRect);
+    }
+
+    public override void WriteSettings()
+    {
+      RSUtility.UpdateRoomSizes();
+
+      base.WriteSettings();
+    }
+
+    public override string SettingsCategory()
+    {
+      return "RSTitle".Translate();
+    }
+
+    private float RoundToNearestHalf(float val)
+    { 
+      return (float)Math.Round(val * 2, MidpointRounding.AwayFromZero) / 2;
+    }
+
+    private float RoundToNearestTenth(float val)
+    {
+      return (float)Math.Round(val * 100, MidpointRounding.AwayFromZero) / 100;
+    }
+  }
+}
---
+++ Source/RoomSize/StaticConstructor.cs
@@ -0,0 +1,46 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using System.Linq;
+using Verse;
+
+namespace RoomSize
+{
+  [StaticConstructorOnStartup]
+  public static class RSStartUp
+  {
+    static RSStartUp()
+    {
+      // Loads right before main menu
+      RSUtility.UpdateRoomSizes();
+    }
+  }
+
+  public class RSUtility
+  {
+    public static void UpdateRoomSizes()
+    {
+      //DefDatabase<HediffDef>.GetNamed("SmokeleafHigh").stages[0].capMods[0].offset = LLLModSettings.amountPenaltyConsciousness;
+      //HediffDef.Named("SmokeleafHigh").stages.Where((HediffStage stage) => stage.capMods.Any((PawnCapacityModifier mod) => mod.capacity == PawnCapacityDefOf.Consciousness)).First().capMods.Where((PawnCapacityModifier mod) => mod.capacity == PawnCapacityDefOf.Consciousness).First().offset = RSModSettings.amountCramped;
+      foreach(RoomStatScoreStage stage in DefDatabase<RoomStatDef>.GetNamed("Space").scoreStages)
+      {
+        if (stage.label == "rather tight")
+          stage.minScore = RSModSettings.amountRatherTight;
+
+        if (stage.label == "average-sized")
+          stage.minScore = RSModSettings.amountAverageSized;
+
+        if (stage.label == "somewhat spacious")
+          stage.minScore = RSModSettings.amountSomewhatSpacious;
+
+        if (stage.label == "quite spacious")
+          stage.minScore = RSModSettings.amountQuiteSpacious;
+
+        if (stage.label == "very spacious")
+          stage.minScore = RSModSettings.amountVerySpacious;
+
+        if (stage.label == "extremely spacious")
+          stage.minScore = RSModSettings.amountExtremelyspacious;
+      }
+    }
+  }
+}
